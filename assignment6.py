7a55a2574b0cc3c6799603c4d1dccc8bfd518d31
Stefan Eng <stefaneng13@gmail.com>
2016-03-02 10:31:40 -0800

    Generate baskets as a matrix

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,6 @@
-# Problem 6.1.1
+import numpy as np
 
+# Problem 6.1.1
 def generate_baskets(n=100):
     all_buckets = []
     for bucket_i in range(1, n + 1):
@@ -10,4 +11,14 @@ def generate_baskets(n=100):
         all_buckets.append(curr_bucket)
     return all_buckets
 
-print generate_baskets(100)
+def generate_baskets_matrix(n=100):
+    # Makes an n x n matrix and uses 1 to represent in the bucket
+    # Rows are buckets, and columns are items
+    baskets_matrix = np.zeros((n,n))
+    for bucket_i in range(1, n + 1):
+        for item_i in range(1, bucket_i + 1):
+            if bucket_i % item_i == 0:
+                baskets_matrix[bucket_i - 1, item_i - 1] = 1
+    return baskets_matrix
+
+print generate_baskets_matrix(100)
